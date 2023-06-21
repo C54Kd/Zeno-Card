dfc0c43f2dc9a14d363d58d5830df83de3bf02c5
kk <ei@google.com>
2023-06-21 15:15:49 +0900

    modal_ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,15 +9,15 @@ export default function Navbar() {
   const dispatch = useDispatch();
 
   return (
-    <header className="bg-hColor flex items-center">
+    <header className="bg-hColor flex items-center h-16">
       <Link to="/">
-        <AiTwotoneHome className="text-1xl text-dColor ml-2" />
+        <AiTwotoneHome className="text-2xl text-dColor ml-4" />
       </Link>
-      <h1 className="text-3xl font-bold text-dColor grow text-center">
-        Zeno-Card
+      <h1 className="text-4xl font-bold text-dColor grow text-center font-serif">
+        ZENO_CARD
       </h1>
 
-      <BsQuestionSquareFill className="text-1xl text-dColor mr-2" onClick={()=>{
+      <BsQuestionSquareFill className="text-2xl text-dColor mr-4" onClick={()=>{
         dispatch(onModal())
       }} />
     </header>
--- src/features/modal/Modal.js
+++ src/features/modal/Modal.js
@@ -1,14 +1,38 @@
-import React from 'react';
-
+import React from "react";
+import "./Modal.css";
+import { useDispatch } from "react-redux";
+import { onModal } from "./modalSlice";
 
 export default function Modal() {
-
-
-
-    return (
-        <div>
-            modal!
+  const dispatch = useDispatch();
+  return (
+    <>
+      <div
+        className="back"
+        onClick={() => {
+          dispatch(onModal());
+        }}
+      ></div>
+      <div className="modalBody">
+        <p className="title">👻 Game Rule 🤔</p>
+        <div className="script">
+          <ol>1. 게임 상황은 실제 날씨에 따라 달라집니다.</ol>
+          <ol>2. 매 턴마다 상황카드가 주어집니다. </ol>
+          <ol>3. 플레이어는 두 가지 선택지 중 하나를 선택해야 합니다.</ol>
+          <ol>4. 상단에는 네 가지 상태창이 있습니다.</ol>
+          <ol>5. 플레이어의 선택에 따라 상태창이 변화합니다.</ol>
+          <ol>6. 네 가지 상태창 중 하나라도 0이 되면 게임 오버.</ol>
         </div>
-    );
-}
 
+        <button
+          className="close"
+          onClick={(e) => {
+            dispatch(onModal());
+          }}
+        >
+          Close
+        </button>
+      </div>
+    </>
+  );
+}
