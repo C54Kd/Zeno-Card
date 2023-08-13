20cfd7aeae5e8e57cdf2960dc9c84586f634cc30
kk <Ei258CQ@gmail.com>
2023-08-13 23:23:19 +0900

    network fix

--- src/index.js
+++ src/index.js
@@ -1,45 +1,39 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import NotFound from './pages/NotFound';
-import Landing from './pages/Landing';
-import Game from './pages/Game';
-import ChooseWeather from './pages/ChooseWeather';
-import Score from './pages/Score';
-import CurrentWeather from './pages/CurrentWeather';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import NotFound from "./pages/NotFound";
+import Landing from "./pages/Landing";
+import Game from "./pages/Game";
+import ChooseWeather from "./pages/ChooseWeather";
+import Score from "./pages/Score";
+import CurrentWeather from "./pages/CurrentWeather";
 
-
-
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <NotFound />,
     children: [
-      {index:true, element: <Landing />},
-      {path: 'choose', element: <ChooseWeather /> },
-      {path: 'current', element: <CurrentWeather /> },
-      {path: 'game', element: <Game />},
-      {path: 'score', element:<Score />}
-    ]
-  
-
-  }
-]) 
+      { index: true, element: <Landing /> },
+      { path: "choose", element: <ChooseWeather /> },
+      { path: "current", element: <CurrentWeather /> },
+      { path: "game", element: <Game /> },
+      { path: "score", element: <Score /> },
+    ],
+  },
+]);
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/CurrentWeather.jsx
+++ src/pages/CurrentWeather.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./CurrentWeather.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -7,31 +7,32 @@ import { changeWeather } from "../features/curWeather/curWeatherSlice";
 import { changeStat } from "../features/stat/statSlice";
 
 export default function CurrentWeather() {
-    const dispatch = useDispatch();
-    const [weatherNow, setWeatherNow] = useState("");
-  const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY
-  axios
-    .get(
-      `https://api.openweathermap.org/data/2.5/weather?lat=37.5&lon=127&appid=${apiKey}`
-    )
-    .then((res) => {
-      let weatherCode = res.data.weather[0].id;
-      let codeNum = (weatherCode+'')[0];
-      let curWeather;
-      if(codeNum === '2' || codeNum === '3' || codeNum === '5') curWeather = '비';
-      else if(codeNum === '6') curWeather = '눈';
-      else if(codeNum === '7' || codeNum === '8') curWeather = '구름';
-
-      setWeatherNow(curWeather);
-    }).catch(err=>console.log(err));
-
-
-
+  const dispatch = useDispatch();
+  const [weatherNow, setWeatherNow] = useState();
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_OPENWEATHER_API_KEY;
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=37.5&lon=127&appid=${apiKey}`
+      )
+      .then((res) => {
+        let weatherCode = res.data.weather[0].id;
+        let codeNum = (weatherCode + "")[0];
+        let curWeather;
+        if (codeNum === "2" || codeNum === "3" || codeNum === "5")
+          curWeather = "비";
+        else if (codeNum === "6") curWeather = "눈";
+        else if (codeNum === "7" || codeNum === "8") curWeather = "구름";
+        setWeatherNow(curWeather);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  
 
   const navigate = useNavigate();
   const goToGamePage = () => {
     dispatch(changeWeather(weatherNow));
-    dispatch(changeStat([70,70,70,70]));
+    dispatch(changeStat([70, 70, 70, 70]));
     navigate("/game");
   };
 
